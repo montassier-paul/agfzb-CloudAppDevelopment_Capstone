c070244b78ad8e8f1305c12bf10440888172031d
montassier-paul <99366674+montassier-paul@users.noreply.github.com>
2022-07-12 16:26:27 +0200

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 APPEND_SLASH = True
 
 ALLOWED_HOSTS = ['localhost',"localhost:8000", "rwA9.eu-gb.cf.appdomain.cloud"]
+CSRF_TRUSTED_ORIGINS=["https://montassierpa-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+
 
 
 # Application definition
