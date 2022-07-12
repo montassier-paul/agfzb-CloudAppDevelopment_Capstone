a9dec4e03b9ea25fc065e95196686f133aecd888
montassier-paul <99366674+montassier-paul@users.noreply.github.com>
2022-07-12 20:53:30 +0200

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 APPEND_SLASH = True
 
 
-ALLOWED_HOSTS = ['localhost',"localhost:8000", "rwA9.eu-gb.cf.appdomain.cloud", "montassierpa-8000.theiadocker-4-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
-CSRF_TRUSTED_ORIGINS=["https://montassierpa-8000.theiadocker-4-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",'https://rwa9.eu-gb.cf.appdomain.cloud/djangoapp/', 'https://*.127.0.0.1']
+ALLOWED_HOSTS = ['localhost',"localhost:8000", "rwA9.eu-gb.cf.appdomain.cloud", "montassierpa-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai", "montassierpa-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+CSRF_TRUSTED_ORIGINS=["https://montassierpa-8000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai", "https://montassierpa-8000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",'https://rwa9.eu-gb.cf.appdomain.cloud/djangoapp/', 'https://*.127.0.0.1']
 
 
 
